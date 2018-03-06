acd5c3be087d47d95c4048b42940f5cf86c9e081
dmelehov <dmelehov1986@gmail.com>
2018-03-06 17:11:34 +0200

    bytecode_help.c normed

--- bytecode_help.c
+++ bytecode_help.c
@@ -3,20 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   bytecode_help.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dshevche <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: dmelehov <dmelehov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 13:57:56 by dshevche          #+#    #+#             */
-/*   Updated: 2018/03/06 13:57:57 by dshevche         ###   ########.fr       */
+/*   Updated: 2018/03/06 17:11:11 by dmelehov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "asm.h"
 
-
-int 			ft_is_need_arg_code(char *inst)
+int			ft_is_need_arg_code(char *inst)
 {
-	int 		i;
+	int	i;
 
 	i = 0;
 	ft_strcmp(inst, "live") == 0 ? i++ : 0;
@@ -29,16 +27,15 @@ int 			ft_is_need_arg_code(char *inst)
 		return (0);
 }
 
-
-int		ft_spec_case(char *inst, int n)
+int			ft_spec_case(char *inst, int n)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	((ft_strcmp(inst, "sti") == 0 && (n == 2 || n == 3)) ? i++ : 0);
 	((ft_strcmp(inst, "ldi") == 0 && (n == 1 || n == 2)) ? i++ : 0);
 	((ft_strcmp(inst, "lldi") == 0 && (n == 1 || n == 2)) ? i++ : 0);
-	ft_strcmp(inst, "fork") == 0  ? i++ : 0;
+	ft_strcmp(inst, "fork") == 0 ? i++ : 0;
 	((ft_strcmp(inst, "zjmp") == 0)) ? i++ : 0;
 	if (i != 0)
 		return (1);
@@ -46,8 +43,7 @@ int		ft_spec_case(char *inst, int n)
 		return (0);
 }
 
-
-int		ft_get_max(line_list *list)
+int			ft_get_max(line_list *list)
 {
 	if (list == NULL)
 		return (0);
