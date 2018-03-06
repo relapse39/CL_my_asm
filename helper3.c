dbfe7ed38e2cee933a5605322a9aa8418c9dec1b
dmelehov <dmelehov1986@gmail.com>
2018-03-06 16:45:18 +0200

    helper3.c normed

--- helper3.c
+++ helper3.c
@@ -1,24 +1,31 @@
-//
-// Created by Daniil Shevchenko on 3/6/18.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   helper3.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmelehov <dmelehov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/06 16:12:39 by dmelehov          #+#    #+#             */
+/*   Updated: 2018/03/06 16:44:59 by dmelehov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "asm.h"
 
-int 	ft_check_lables(line_list *list)
+int		ft_check_lables(line_list *list)
 {
-	int     i;
+	int	i;
 
-
-	while(list)
+	while (list)
 	{
 		i = -1;
-		while(list->ent.com[++i])
+		while (list->ent.com[++i])
 		{
 			if (list->ent.com[i] != NULL)
 			{
 				if ((list->ent.com[i][0] == ':') ||
 					(list->ent.com[i][0] == '%'
-					 && list->ent.com[i][1] == ':'))
+					&& list->ent.com[i][1] == ':'))
 					if (ft_check_label_char(list, i) == -1)
 						return (-1);
 			}
