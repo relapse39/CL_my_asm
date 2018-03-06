ac9c133e3c88902bc397fa9b471c25bfb5a0dfec
dmelehov <dmelehov1986@gmail.com>
2018-03-06 17:07:24 +0200

    clear_file.c dorabotat

--- clear_file.c
+++ clear_file.c
@@ -3,13 +3,12 @@
 /*                                                        :::      ::::::::   */
 /*   clear_file.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dshevche <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: dmelehov <dmelehov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 14:00:58 by dshevche          #+#    #+#             */
-/*   Updated: 2018/03/06 14:00:59 by dshevche         ###   ########.fr       */
+/*   Updated: 2018/03/06 17:06:54 by dmelehov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "asm.h"
 
@@ -17,17 +16,17 @@ int			ft_is_delete(char **com)
 {
 	if (com[0] == NULL)
 		return (1);
-	if (com[0] != NULL  && (ft_strcmp(com[0], NAME_CMD_STRING) == 0
-							|| ft_strcmp(com[0], COMMENT_CMD_STRING) == 0
-							|| ft_is_label(com[0])))
+	if (com[0] != NULL && (ft_strcmp(com[0], NAME_CMD_STRING) == 0
+				|| ft_strcmp(com[0], COMMENT_CMD_STRING) == 0
+				|| ft_is_label(com[0])))
 		return (1);
 	else
 		return (0);
 }
 
-line_list   	*ft_remove(line_list *list)
+line_list	*ft_remove(line_list *list)
 {
-	int 	i;
+	int	i;
 
 	i = -1;
 	if (list == NULL)
@@ -37,7 +36,7 @@ line_list   	*ft_remove(line_list *list)
 		line_list *tmp;
 		tmp = list->next;
 		free(list->ent.r_line);
-		while(list->ent.com[++i])
+		while (list->ent.com[++i])
 			free(list->ent.com[i]);
 		free(list->ent.com[i]);
 		free(list->ent.com);
@@ -46,12 +45,11 @@ line_list   	*ft_remove(line_list *list)
 	}
 	list->next = ft_remove(list->next);
 	return (list);
-
 }
 
-int 		ft_got_thresh(line_list *list)
+int			ft_got_thresh(line_list *list)
 {
-	while(list)
+	while (list)
 	{
 		if (ft_is_delete((list->ent.com)))
 			return (1);
@@ -60,14 +58,13 @@ int 		ft_got_thresh(line_list *list)
 	return (-1);
 }
 
-
-
 void		ft_clear(line_list *list)
 {
-	int i;
-	int j;
-	int k;
-	char *new_s = NULL;
+	int		i;
+	int		j;
+	int		k;
+	char 	*new_s = NULL;
+
 	while (list)
 	{
 		i = 0;
@@ -75,19 +72,19 @@ void		ft_clear(line_list *list)
 		if (ft_is_label(list->ent.com[0]) &&
 				list->ent.com[1] != NULL)
 		{
-			while(list->ent.com[i] != NULL)
+			while (list->ent.com[i] != NULL)
 			{
-				while(list->ent.com[i][j] != 0)
+				while (list->ent.com[i][j] != 0)
 					j++;
 				i++;
 			}
 			new_s = malloc(sizeof(j + i));
 			i = 1;
 			k = 0;
-			while(list->ent.com[i] != NULL)
+			while (list->ent.com[i] != NULL)
 			{
 				j = 0;
-				while(list->ent.com[i][j] != 0)
+				while (list->ent.com[i][j] != 0)
 				{
 					new_s[k] = list->ent.com[i][j];
 					j++;
@@ -99,7 +96,7 @@ void		ft_clear(line_list *list)
 			}
 			new_s[k] = '\0';
 			i = 0;
-			while(list->ent.com[i])
+			while (list->ent.com[i])
 			{
 				free(list->ent.com[i]);
 				i++;
@@ -108,8 +105,6 @@ void		ft_clear(line_list *list)
 			free(list->ent.com);
 			list->ent.com = ft_strsplit(new_s, ' ');
 			free(new_s);
-
-
 		}
 		list = list->next;
 	}
@@ -117,28 +112,7 @@ void		ft_clear(line_list *list)
 
 void		ft_clear_list(line_list **list)
 {
-//	int 	i;
-//	line_list *tmp;
-//
-//	tmp = *list;
-//	while (tmp)
-//	{
-//		i = -1;
-//		if (ft_is_label(tmp->entry.com[0]) &&
-//			tmp->entry.com[1] != NULL)
-//		{
-//			while(tmp->entry.com[++i] && tmp->entry.com[i + 1])
-//			{
-//				ft_strdel(&tmp->entry.com[i]);
-//				tmp->entry.com[i] = ft_strdup(tmp->entry.com[i + 1]);
-//
-//			}
-//			ft_strdel(&tmp->entry.com[i]);
-//		}
-//		tmp = tmp->next;
-//	}
 	ft_clear(*list);
 	while (ft_got_thresh(*list) == 1)
 		*list = ft_remove(*list);
-
 }
