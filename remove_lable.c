e6e04aa79710781e7e8b173d070655c8c71dc072
dshevche <relapse39@gmail.com>
2018-03-06 16:04:51 +0200

    norm

--- remove_lable.c
+++ remove_lable.c
@@ -6,24 +6,17 @@
 /*   By: dshevche <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 14:02:09 by dshevche          #+#    #+#             */
-/*   Updated: 2018/03/06 14:02:12 by dshevche         ###   ########.fr       */
+/*   Updated: 2018/03/06 16:04:03 by dshevche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "asm.h"
 
-
-
-
-
-
-
-int 	ft_is_duplicate(line_list *list)
+int				ft_is_duplicate(line_list *list)
 {
-	line_list *tmp;
+	line_list	*tmp;
 
-	while(list)
+	while (list)
 	{
 		tmp = list->next;
 		while (tmp)
@@ -31,36 +24,36 @@ int 	ft_is_duplicate(line_list *list)
 			if (ft_is_label(tmp->ent.com[0])
 				&& ft_strequ(list->ent.com[0], tmp->ent.com[0]))
 			{
-				ft_putstr("error: this label defined more than one time: '");
-				ft_putstr(tmp->ent.com[0]);
-				ft_putstr("'\n");
+				ft_printf("error: this label defined more than one time: '");
+				ft_printf("%s\n", tmp->ent.com[0]);
 				return (-1);
 			}
-		tmp = tmp->next;
+			tmp = tmp->next;
 		}
 		list = list->next;
 	}
 	return (0);
 }
 
-char *ft_get_bytes2(line_list *list, line_list *tmp)
+char			*ft_get_bytes2(line_list *list, line_list *tmp)
 {
-	char *ret;
-	char *tmp_s;
+	char		*ret;
+	char		*tmp_s;
+
 	tmp_s = ft_itoa(list->ent.pos - tmp->ent.pos);
 	ret = malloc(sizeof(ft_strlen(tmp_s) + 1));
 	ret[0] = '%';
 	ret[1] = '\0';
-	ret = ft_strcat(ret,tmp_s);
+	ret = ft_strcat(ret, tmp_s);
 	ret[ft_strlen(ret) + 1] = '\0';
 	free(tmp_s);
 	return (ret);
 }
 
-char *ft_get_byte(line_list *list, line_list *tmp, int i, char *err)
+char			*ft_get_byte(line_list *list, line_list *tmp, int i, char *err)
 {
-	char *cmd;
-	int 	flag;
+	char		*cmd;
+	int			flag;
 
 	flag = 0;
 	tmp->ent.com[i][0] == ':' ? flag = 1 : (0);
@@ -78,30 +71,28 @@ char *ft_get_byte(line_list *list, line_list *tmp, int i, char *err)
 		}
 		list = list->next;
 	}
-    ft_printf("No such label %s", err + 1);
-    ft_printf(" while attempting to dereference token [TOKEN]");
-    ft_printf("[%03d:", tmp->ent.nbr);
-    ft_printf("%d]", ft_get_pos(tmp->ent.raw_line, err) + 1);
-    err[0] == '%' ? ft_printf(" ") :ft_printf(" IN");
-    ft_printf("DIRECT_LABEL %s", err);
+	ft_printf("No such label %s", err + 1);
+	ft_printf(" while attempting to dereference token [TOKEN]");
+	ft_printf("[%03d:%d]", tmp->ent.nbr, ft_get_pos(tmp->ent.r_line, err) + 1);
+	err[0] == '%' ? ft_printf(" ") : ft_printf(" IN");
+	ft_printf("DIRECT_LABEL %s", err);
 	return (NULL);
 }
 
-int 	ft_redirect_lables(line_list *list)
+int				ft_redirect_lables(line_list *list)
 {
-	line_list *tmp;
-	int 	i;
-	char 	*error_c;
+	line_list	*tmp;
+	int			i;
+	char		*error_c;
 
 	tmp = list;
 	while (tmp)
 	{
 		i = -1;
-		while(tmp->ent.com[++i])
+		while (tmp->ent.com[++i])
 		{
-            if ((tmp->ent.com[i] && tmp->ent.com[i][0] == ':'
-				&& tmp->ent.com[i][1])
-				|| (tmp->ent.com[i][0] == '%'
+			if ((tmp->ent.com[i] && tmp->ent.com[i][0] == ':'
+				&& tmp->ent.com[i][1]) || (tmp->ent.com[i][0] == '%'
 					&& tmp->ent.com[i][1] == ':' && tmp->ent.com[i][2]))
 			{
 				error_c = ft_strdup(tmp->ent.com[i]);
@@ -117,9 +108,8 @@ int 	ft_redirect_lables(line_list *list)
 	return (0);
 }
 
-int 	ft_remove_labels(line_list *list)
+int				ft_remove_labels(line_list *list)
 {
-
 	if (ft_check_lables(list) == -1)
 		return (-1);
 	if (ft_is_duplicate(list) == -1)
