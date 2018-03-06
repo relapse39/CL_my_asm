a83940ef150de49d6eaa4ff858a1edfa6566a8c5
dmelehov <dmelehov1986@gmail.com>
2018-03-06 16:22:17 +0200

    validate.c normed but 1 ft should be splited

--- validate.c
+++ validate.c
@@ -3,19 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   validate.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dshevche <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: dmelehov <dmelehov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 14:02:30 by dshevche          #+#    #+#             */
-/*   Updated: 2018/03/06 14:02:32 by dshevche         ###   ########.fr       */
+/*   Updated: 2018/03/06 16:19:30 by dmelehov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "asm.h"
 
-int 	ft_find_inst(char *inst)
+int		ft_find_inst(char *inst)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (g_op_tab[i].name && ft_strequ(g_op_tab[i].name, inst) != 1)
@@ -26,52 +25,50 @@ int 	ft_find_inst(char *inst)
 		return (i);
 }
 
-
-
 int		ft_check_args_n(line_list *list)
 {
-	int par;
+	int	par;
 
 	par = ft_find_inst(list->ent.com[0]);
-		if (par == -1)
-		{
-			ft_printf("Invalid instruction at token [TOKEN]");
-			ft_printf("[%03d",list->ent.nbr);
-			ft_printf(":%03d]",ft_get_pos(list->ent.r_line, list->ent.com[0]) + 1);
-			ft_printf(" %s \"%s\"",ft_get_l_i_str(list->ent.com[0]), list->ent.com[0]);
-			return (-1);
-		}
-		if (g_op_tab[par].arg_num != list->ent.args_n)
-		{
-			ft_printf("Invalid parameter %d type ",g_op_tab[par].arg_num - 1);
-			ft_printf("register for instruction %s ",g_op_tab[par].name);
-			//ft_printf("bad nbr. of args in line: %s", list->entry.raw_line);
-			return (-1);
-		}
+	if (par == -1)
+	{
+		ft_printf("Invalid instruction at token [TOKEN]");
+		ft_printf("[%03d", list->ent.nbr);
+		ft_printf(":%03d]", ft_get_pos(list->ent.r_line, list->ent.com[0]) + 1);
+		ft_printf(" %s \"%s\"",
+			ft_get_l_i_str(list->ent.com[0]), list->ent.com[0]);
+		return (-1);
+	}
+	if (g_op_tab[par].arg_num != list->ent.args_n)
+	{
+		ft_printf("Invalid parameter %d type ", g_op_tab[par].arg_num - 1);
+		ft_printf("register for instruction %s ", g_op_tab[par].name);
+		return (-1);
+	}
 	return (0);
 }
 
-int 	ft_print_err_inst(line_list *list, int n)
+int		ft_print_err_inst(line_list *list, int n)
 {
 	ft_printf("Syntax error in token [TOKEN]");
-	ft_printf("[%03d:",list->ent.nbr);
-	ft_printf("%03d", ft_get_pos(list->ent.r_line,list->ent.com[n]) + 1);
-	ft_printf("] INSTRUCTION \"%s\"",list->ent.com[n]);
+	ft_printf("[%03d:", list->ent.nbr);
+	ft_printf("%03d", ft_get_pos(list->ent.r_line, list->ent.com[n]) + 1);
+	ft_printf("] INSTRUCTION \"%s\"", list->ent.com[n]);
 	return (-1);
 }
 
-int 	ft_print_err_lexical(line_list *list, int n)
+int		ft_print_err_lexical(line_list *list, int n)
 {
 	ft_printf("Lexical error at ");
-	ft_printf("[%d:",list->ent.nbr);
-	ft_printf("%d]", ft_get_pos(list->ent.r_line,list->ent.com[n]) + 1);
+	ft_printf("[%d:", list->ent.nbr);
+	ft_printf("%d]", ft_get_pos(list->ent.r_line, list->ent.com[n]) + 1);
 	return (-1);
 }
 
-int 	ft_check_arg(line_list *list, int n)
+int		ft_check_arg(line_list *list, int n)
 {
-	int i;
-	int type;
+	int	i;
+	int	type;
 
 	type = -1;
 	n == 1 ? type = list->ent.f_type : (0);
@@ -79,103 +76,33 @@ int 	ft_check_arg(line_list *list, int n)
 	n == 3 ? type = list->ent.t_type : (0);
 	i = ft_find_inst(list->ent.com[0]);
 	if (type != T_DIR && type != T_REG && type != T_IND && type != INST)
-		return(ft_print_err_lexical(list, n));
+		return (ft_print_err_lexical(list, n));
 	if (type == INST)
 		return (ft_print_err_inst(list, n));
-
-
-
-
 	if ((g_op_tab[i].args[n - 1] & type) == 0)
 	{
-		ft_printf("Invalid parameter %d type register",n - 1);
-		ft_printf(" for instruction %s",g_op_tab[i].name);
+		ft_printf("Invalid parameter %d type register", n - 1);
+		ft_printf(" for instruction %s", g_op_tab[i].name);
 		return (-1);
 	}
-	if(type == T_REG)
+	if (type == T_REG)
 	{
-		if(ft_atoi(list->ent.com[n] + 1) > REG_NUMBER)
+		if (ft_atoi(list->ent.com[n] + 1) > REG_NUMBER)
 		{
 			ft_printf("Syntax error in token [TOKEN]");
-			ft_printf("[%03d:",list->ent.nbr);
-			ft_printf("%03d", ft_get_pos(list->ent.r_line,list->ent.com[n]) + 1);
-			ft_printf("] INSTRUCTION \"%s\"",list->ent.com[n]);
+			ft_printf("[%03d:", list->ent.nbr);
+			ft_printf("%03d", ft_get_pos(list->ent.r_line,
+				list->ent.com[n]) + 1);
+			ft_printf("] INSTRUCTION \"%s\"", list->ent.com[n]);
 			return (-1);
 		}
 	}
-
-//	if (ft_atoi(arg + 1) == 0 || ft_atoi(arg + 1) > REG_NUMBER)
-//	{
-//		//ft_printf("r is too big at line %s", line);
-//		return (-1);
-//	}
 	return (0);
 }
 
-//int 	ft_check_dir(char *arg, line_list *list, int n)
-//{
-//	int	i;
-//
-//	if (arg[1] == '-')
-//		i = 2;
-//	else
-//		i = 1;
-//	while (arg[i])
-//	{
-//		if (!ft_isdigit(arg[i]))
-//		{
-//			//ft_printf("bad dir arg '%s' in line: %s",arg[i], line);
-//			return (-1);
-//		}
-//		i++;
-//	}
-//	if (ft_strlen(arg + 1) > 10 )
-//	{
-//		//ft_printf("dir is to big at line %s", line);
-//	}
-//	return (0);
-//}
-//
-//int ft_check_indir(char *arg, line_list *list, int n)
-//{
-//	int i;
-//
-//	if (arg[0] == '-')
-//		i = 1;
-//	else
-//		i = 0;
-//	while (arg[i])
-//	{
-//		if (!ft_isdigit(arg[i]))
-//		{
-//			//ft_printf("bad indir arg in line%s", line);
-//			return (-1);
-//		}
-//		i++;
-//	}
-//	if ((ft_atoi(arg) > IDX_MOD))
-//	{
-//		//ft_printf("too big indir arg in line%s , %d", line, ft_atoi(arg));
-//		return (-1);
-//	}
-//	return (0);
-//}
-
-
-//int 	ft_check_args_1(line_list *list, int type, int n)
-//{
-//	type = 0;
-//		return (ft_check_reg( list, n));
-//	else if (type == 2)
-//		return(ft_check_dir(arg, list, n));
-//	else if (type == 3)
-//		return (ft_check_indir(arg, list, n));
-//	return (0);
-//}
-
-int 	ft_cycle_body(line_list *list)
+int		ft_cycle_body(line_list *list)
 {
-	int res;
+	int	res;
 
 	res = 0;
 	if (list->ent.f_type != 0)
@@ -189,14 +116,12 @@ int 	ft_cycle_body(line_list *list)
 	return (0);
 }
 
-
-
-int 	ft_is_valid(line_list *list)
+int		ft_is_valid(line_list *list)
 {
-	line_list *tmp;
+	line_list	*tmp;
 
 	tmp = list;
-	while(tmp)
+	while (tmp)
 	{
 		if (ft_check_args_n(tmp) == -1)
 			return (-1);
